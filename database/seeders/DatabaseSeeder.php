dd41cca2b50e437744c03991c664f25dc4c20e28
IvanGostev <ivangostev07@gmail.com>
2026-01-21 18:12:37 +0300

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             FkkoSeeder::class,
+            PageSeeder::class,
         ]);
     }
 }
